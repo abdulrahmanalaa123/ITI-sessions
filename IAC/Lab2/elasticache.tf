26475689e3db0bd5f5a5653141f7015a3ea72ab1
abdelrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2025-03-03 17:28:42 +0200

    udpating log on the date of:2025-03-03

--- IAC/Lab2/elasticache.tf
+++ IAC/Lab2/elasticache.tf
@@ -1,1 +1,15 @@
- 
+resource "aws_elasticache_cluster" "example" {
+  cluster_id           = "cluster-example"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 3000
+  security_group_ids = [aws_security_group.private_ssh.id]
+}
+
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "tf-test-redis-subnet"
+  subnet_ids = [aws_subnet.subnets["elasticache_subnet"].id]
+}
